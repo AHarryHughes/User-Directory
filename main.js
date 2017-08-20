2f53218b1764b5e48d4185a6005ba0a49402dc8f
AHarryHughes <Ahhughes8@gmail.com>
2017-08-20 15:20:37 -0400

    employed and unemployed

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser');
 var robots = require('./models/robots.js');
 //var data = {};
 //data.list = require('./data.js');
@@ -8,6 +9,9 @@ mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/robots');
 
 var application = express();
+
+application.use(bodyParser());
+application.use(bodyParser.urlencoded({ extended: true }));
 
 application.engine('mustache', mustacheExpress());
 
@@ -23,15 +27,38 @@ application.get('/', async function (request, response) {
     response.render('index', {daRobots: daRobots});
 });
 
-application.get('/:x', async function (request, response) {
+application.get('/userdata/:x', async function (request, response) {
     //response.render('user_data', data.list.users[request.params.x - 1]);
     //search through mongodb for user by id
     let id = request.params.x;
     let robot = await robots.find({
         "id": id
     });
-    console.log(robot);
     response.render('user_data', {robot: robot[0]});
+});
+
+application.get('/employeeSelect', async function (request, response) {
+    //return robots based on select
+    console.log('/employeeSelect');
+    let selectValue = request.query.employeeType;
+    if(selectValue == "Unemployed"){
+        console.log("Unemployed");
+        let daRobots = await robots.find({
+                job: null
+        });
+        response.render('index', {daRobots: daRobots});
+    }
+    else if(selectValue == "Employed"){
+        console.log("Employed");
+        let daRobots = await robots.find({
+                job: {$ne:null}
+        });
+        response.render('index', {daRobots: daRobots});
+    }
+    else{
+        console.log("else");
+        response.redirect('/');
+    }
 });
 
 application.listen(4000);
